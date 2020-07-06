422907c9f2645fe591b4cc80a140568e19f5e047
Leandro Sommese <>
2020-07-06 01:06:53 -0300

    apertura y cierre de imagenes

--- lib/image/image_data.py
+++ lib/image/image_data.py
@@ -6,7 +6,9 @@ import numpy as np
 # Returns 2d matrix with the intensity of each pixel in each position
 # For smoothing we are using a bilateral filter https://docs.opencv.org/2.4/modules/imgproc/doc/filtering.html#bilateralfilter
 def get_image_data(image_name):
-    image_matrix = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+    import cv2
+    image_matrix = cv2.imread(image_name)
+    image_matrix = cv2.cvtColor(image_matrix, cv2.COLOR_BGR2GRAY)
     if image_matrix is None:
         raise ValueError(f'Image {image_name} not found')
 
@@ -14,8 +16,8 @@ def get_image_data(image_name):
     return cv2.bilateralFilter(image_matrix, 20, 300, 300)
 
 
-def crop_vertical(iamge_matrix, pixel_start, pixel_end):
-    return iamge_matrix[pixel_start:pixel_end]
+def crop_vertical(image_matrix, pixel_start, pixel_end):
+    return image_matrix[pixel_start:pixel_end]
 
 
 def crop_horizontal(image_matrix, pixel_start, pixel_end):
@@ -29,6 +31,7 @@ def crop_horizontal(image_matrix, pixel_start, pixel_end):
 # split_vertically_by[[1,2,3,4], [5,6,7,8]], 2) => [[[1,2],[5,6]], [[3,4],[7,8]]]
 # split_vertically_by[[1,2,3], [5,6,7]], 2) => [[[1,2],[5,6]], [[3],[7]]]
 def split_vertically_by(image_matrix, slice_width):
+    import numpy as np
     if slice_width > len(image_matrix[0]): raise ValueError("The value of the slice width needs to be smaller than the len of the x axis")
 
     image_width = len(image_matrix[0])
