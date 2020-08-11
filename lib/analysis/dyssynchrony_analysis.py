cf0aeee78ec099c8c2ebc7c514290e11994ce18c
agusgs <garciasmithagustin@gmail.com>
2020-08-11 09:00:58 -0300

    add tau calculation to dyssynchrony analysis

--- lib/analysis/dyssynchrony_analysis.py
+++ lib/analysis/dyssynchrony_analysis.py
@@ -1,11 +1,14 @@
+from math import log
+
 import numpy as np
+from numpy.linalg import lstsq
 from peakutils import indexes
 from lib.image.image_data import split_vertically_by
 
 
-def analyze_image(image, min_dist_between_max_peaks, slice_width=0):
-    image_analysis = _analyze_matrix(image, min_dist_between_max_peaks)
-    slices_analysis = [_analyze_matrix(matrix, min_dist_between_max_peaks) for matrix in
+def analyze_image(image, min_dist_between_max_peaks, calibration, slice_width=0):
+    image_analysis = _analyze_matrix(image, min_dist_between_max_peaks, calibration)
+    slices_analysis = [_analyze_matrix(matrix, min_dist_between_max_peaks, calibration) for matrix in
                        split_vertically_by(image, slice_width)] if slice_width > 0 else []
 
     return ({
@@ -17,7 +20,7 @@ def analyze_image(image, min_dist_between_max_peaks, slice_width=0):
 # PRIVATE FUNCTIONS
 
 
-def _analyze_matrix(matrix, min_dist_between_max_peaks):
+def _analyze_matrix(matrix, min_dist_between_max_peaks, calibration):
     intensities = np.asarray(_mean_columns(matrix), dtype=np.int16)
     max_peaks_positions = _max_peaks_positions(intensities, min_dist_between_max_peaks)
     max_peaks_intensities = _intensities_in_positions(intensities, max_peaks_positions)
@@ -33,9 +36,31 @@ def _analyze_matrix(matrix, min_dist_between_max_peaks):
         "amplitudes": _calculate_amplitudes(max_peaks_intensities, min_peaks_intensities),
         "times_to_peaks": _calculate_time_to_peaks(max_peaks_positions, min_peaks_positions),
         "times_to_half_peaks": _calculate_times_to_half_peaks(intensities, max_peaks_positions, min_peaks_positions),
-        "tau_s": 0
-
+        "tau_s": _calculate_taus(intensities, max_peaks_positions, min_peaks_positions, calibration)
     })
+
+
+def _calculate_tau(times, intensities):
+    x_axis = np.asarray([[1, t] for t in times])
+    y_axis = np.asarray([log(intensity) for intensity in intensities]).T
+    (w, residuals, rank, sing_vals) = lstsq(x_axis, y_axis)
+    tau = -1.0 / w[0]
+    return tau
+
+
+def _calculate_taus(intensities, max_peaks_positions, min_peaks_positions, calibration):
+    taus = []
+    times = np.asarray(range(0, len(intensities))) * calibration
+    for max_min in np.column_stack((max_peaks_positions[0:len(max_peaks_positions)-1], min_peaks_positions)):
+        max_index = int(max_min[0])
+        min_index = int(max_min[1])
+
+        time_between_peaks = np.asarray(times[max_index:min_index])
+        intensities_between_peaks = np.asarray(intensities[max_index:min_index])
+
+        taus.append(_calculate_tau(time_between_peaks, intensities_between_peaks))
+
+    return taus
 
 
 def _calculate_times_to_half_peaks(intensities, max_peaks_positions, min_peaks_positions):
@@ -45,7 +70,7 @@ def _calculate_times_to_half_peaks(intensities, max_peaks_positions, min_peaks_p
         min_index = int(max_min[1])
 
         half_intensity_between_peaks = intensities[max_index] - ((intensities[max_index] - intensities[min_index]) / 2)
-        
+
         half_max_index = 0
         intensities_between_peaks = intensities[min_index:max_index]
 
@@ -61,13 +86,13 @@ def _calculate_times_to_half_peaks(intensities, max_peaks_positions, min_peaks_p
 
 def _calculate_time_to_peaks(max_peaks_positions, min_peaks_positions):
     return [max_min[0] - max_min[1] for max_min in
-     np.column_stack((max_peaks_positions[1:len(max_peaks_positions)], min_peaks_positions,))]
+            np.column_stack((max_peaks_positions[1:len(max_peaks_positions)], min_peaks_positions,))]
 
 
 def _calculate_amplitudes(max_peaks_intensities, min_peaks_intensities):
     amplitudes = []
     for i in range(0, len(min_peaks_intensities)):
-        amplitude = (max_peaks_intensities[i+1] - min_peaks_intensities[i]) / min_peaks_intensities[i]
+        amplitude = (max_peaks_intensities[i + 1] - min_peaks_intensities[i]) / min_peaks_intensities[i]
         amplitudes.append(amplitude)
     return amplitudes
 
