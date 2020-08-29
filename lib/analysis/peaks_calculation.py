486842e2cf40f72ab0bd3b6920dd464a63c52f04
Leandro Sommese <sommeseleandro@gmail.com>
2020-08-29 03:54:58 -0300

    analysis of images

--- lib/analysis/peaks_calculation.py
+++ lib/analysis/peaks_calculation.py
@@ -13,7 +13,7 @@ def _max_peaks_positions(vector, min_dist_between_max_peaks):
     threshold = 5.0 / max(vector) if (max(vector) > 0) else 0
     possible_max_peaks = indexes(np.array(vector), thres=threshold, min_dist=min_dist_between_max_peaks)
     intensity_avg = sum(vector) / len(vector)
-    max_peaks = [max_peak for max_peak in possible_max_peaks if vector[max_peak] > intensity_avg/3]
+    max_peaks = [max_peak for max_peak in possible_max_peaks if vector[max_peak] > intensity_avg/4]
 
     if len(max_peaks) == 0: raise ValueError("Peaks not found")
 
