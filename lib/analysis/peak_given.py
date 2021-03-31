a77d4afc8d5dd0eb4bd55f5ccbcef51d07b05b03
leandrosommese <sommeseleandro@gmail.com>
2021-03-31 10:14:21 -0300

    rare peaks treatment

--- lib/analysis/peak_given.py
+++ lib/analysis/peak_given.py
@@ -3,20 +3,30 @@ import numpy as np
 
 
 def calculate_peaks(vector, min_dist_between_max_peaks):
-    max_peaks = [39, 88, 129, 180, 225, 277, 319, 373, 410, 470]
-    min_peaks = [int((list(vector)).index(min (vector)))] * (len(max_peaks) + 1)
+#     max_peaks = [39, 88, 129, 180, 225, 277, 319, 373, 410, 470]
+#     min_peaks = [int((list(vector)).index(min (vector)))] * (len(max_peaks) + 1)
+    min_peaks = _min_peaks_positions(vector, max_peaks)
 
     return max_peaks, min_peaks
 
 
 def _max_peaks_positions(vector, min_dist_between_max_peaks):
-    threshold = 1.0 / max(vector) if (max(vector) > 0) else 0
-    possible_max_peaks = indexes(np.array(vector), thres=threshold, min_dist=min_dist_between_max_peaks)
-    intensity_avg = sum(vector) / len(vector)
-    max_peaks = [max_peak for max_peak in possible_max_peaks if vector[max_peak] > intensity_avg/4]
+    
+    tiempos = []
+    intensidades = []   
+    max_local=0
+    for u in range (1,len(data)-1):
+        if ((data[u]>data[u-1])&(data[u]>data[u+1])):
+            max_local=max_local+1
+            tiempos.append (u)
+            intensidades.append (data[max_local])
+#     threshold = 1.0 / max(vector) if (max(vector) > 0) else 0
+#     possible_max_peaks = indexes(np.array(vector), thres=threshold, min_dist=min_dist_between_max_peaks)
+#     intensity_avg = sum(vector) / len(vector)
+#     max_peaks = [max_peak for max_peak in possible_max_peaks if vector[max_peak] > intensity_avg/4]
 
     if len(max_peaks) == 0: raise ValueError("Peaks not found")
-
+    
     return max_peaks
 
 
